0c989a5301ac64d3176826a078ce44b478ab521f
George Whitehill <jgwhitehill01@Gmail.com>
2023-12-26 20:26:31 -0600

    Added 3x3 matrix control and Macro
    Added 3x3 matrix control for referencing buttons and commands as well as lighting for the slider controller. Added macro button that switches to a toggled alternate command layout.

--- DTP_SLIDER_CONTROLLER/DTP_SLIDER_CONTROLLER.ino
+++ DTP_SLIDER_CONTROLLER/DTP_SLIDER_CONTROLLER.ino
@@ -11,30 +11,24 @@ boolean NL = true;
 
 int input10Pin = A1; 	 // choose the pin for the LED
 int input9Pin = 12;
-int input8Pin = 11;
-int input7Pin = 10;
-int input6Pin = 7;
-int input5Pin = 6;       // define push button input pins
-int input4Pin = 5;
-int input3Pin = 4;
-int input2Pin = 3;
+int input8Pin = 7;
+int input7Pin = 4;
+int input6Pin = 11;
+int input5Pin = 5;       // define push button input pins
+int input4Pin = 3;
+int input3Pin = 10;
+int input2Pin = 6;
 int input1Pin = 2;
 
 int buttonRef = 0;
+
+bool macro = false;
 
 #define LED_PIN  13
 #define LED_COUNT 3
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-char *commands[] = {
-  "","",
-  "A", "p40", "x100",
-  "H2", "p30", "p20",
-  "","",
-  "e", "t20", "t40",
-  "R"
-};
 
 uint32_t red = strip.Color(255,0,0);
 uint32_t green = strip.Color(0,255,0);
@@ -45,15 +39,48 @@ uint32_t pink = strip.Color(255,0,100);
 uint32_t cyan = strip.Color(0,255,255);
 uint32_t orange = strip.Color(230,80,0);
 uint32_t off = strip.Color(0,0,0);
-uint32_t* commandColor[] = {
-  0,0,
-  red, green, blue,
-  yellow, white, pink,
-  0,0,
-  cyan, orange, green,
-  off
+
+
+
+char *commands[4][3] = {
+                    {   "A",  "H2",    "e"}, 
+                    {"x100", "t40", "x200"},
+                    { "p20", "t20",  "p40"},
+                    { "R",      "",     ""}
 };
 
+
+char *macroCommands[4][3] = {
+                    //CLEAR_ARRAY,EDIT_ARRAY,EXECUTE_MOVES
+                    {"C", "E", ";"},
+                    //STEP_BACKWARD, ADD_POSITION, STEP_FORWARD
+                    {"<", "#", ">"},
+                    //JUMP_TO_END, ORIBIT_POINT, JUMP_TO_END
+                    {"[", "@", "]"},
+                    { "R", "",  ""}
+};
+
+
+int pinRead[4][3] = {
+                    { input1Pin, input2Pin, input3Pin}, 
+                    { input4Pin, input5Pin, input6Pin},
+                    { input7Pin, input8Pin, input9Pin},
+                    { input10Pin,         14,       14}
+};
+
+uint32_t commandColor[4][3] = {
+                    {  red,orange, cyan}, 
+                    { pink, white, pink},
+                    {green, white,green},
+                    {  off,  blue,  off}
+};
+
+uint32_t macroCommandColor[4][3] = {
+                    { blue, blue, blue}, 
+                    { blue, blue, blue},
+                    { blue, blue, blue},
+                    {  off,  blue,  off}
+};
 
 void setup() 
 {
@@ -85,61 +112,56 @@ void setup()
  
 void loop()
 {
-    for(int i=2 ; i<=13 ; i++) {
-    if (i == 8) {
-      i = 10;
+    for(int y=0 ; y<=4 ; y++) {
+      for(int x=0 ; x<=2 ; x++){
+       // Serial.print(commands[y][x]);
+        checkPush(x,y);
+      }
     }
-
-    checkPush(i);
-    
-    // // Read from the Bluetooth module and send to the Arduino Serial Monitor
-    // if (BTSerial.available())
-    // {
-    //     c = BTSerial.read();
-    //     Serial.write(c);
-    // }
- 
-    // // Read from the Serial Monitor and send to the Bluetooth module
-    // if (Serial.available())
-    // {
-    //     c = Serial.read();
-    //     BTSerial.write(c);   
- 
-    //     // Echo the user input to the main window. The ">" character indicates the user entered text.
-    //     if (NL) { Serial.print(">");  NL = false; }
-    //     Serial.write(c);
-    //     if (c==10) { NL = true; }
-    // }
-     
-    }
-delay(150);
+    delay(100);
 }
 
 void colorWipe(uint32_t color, int wait) {
   for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
     strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-    strip.show();                          //  Update strip to match
-    delay(wait);                           //  Pause for a moment
   }
+  strip.show();                          //  Update strip to match
+
 }
-void checkPush(int pinNumber)
+void checkPush(int buttonX, int buttonY)
 {
   int pushed = LOW;
-  if (pinNumber == 13) {
-  pushed = digitalRead(input10Pin);  // read input value
-  }
-  else {
-  pushed = digitalRead(pinNumber);
-  }
-  if (pushed == HIGH){ // check if the input is HIGH (button released)
-    //digitalWrite(ledPin, HIGH); // turn LED ON
-    Serial.println(commands[pinNumber]);
-    colorWipe(commandColor[pinNumber], 100);
-    BTSerial.write(commands[pinNumber]);
 
+  pushed = digitalRead(pinRead[buttonX][buttonY]);
+
+  if (pushed == HIGH){
+    if (!macro){
+      if (pinRead[buttonX][buttonY] == input10Pin){
+        macro = true;
+        colorWipe(yellow, 100);
+      }
+      else {
+        Serial.println(commands[buttonX][buttonY]);
+        colorWipe(commandColor[buttonX][buttonY], 100);
+      }
+      delay(100);
+    }
+    else if (macro) {
+      macro = false;
+      Serial.println(macroCommands[buttonX][buttonY]);
+      colorWipe(macroCommandColor[buttonX][buttonY], 100);
+      delay(100);
+    }
+    
+    //Serial.println(digitalRead(pinRead[buttonX][buttonY]));
+    //Serial.println(pinRead[buttonX][buttonY]);
+    //BTSerial.write(commands[pinNumber]);
+    
   }
   else{
    // digitalWrite(ledPin, LOW); // turn LED OFF
   }
   pushed = LOW;
 }
+
+
